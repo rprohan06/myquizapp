61ab8a3952a048e46b1a235806c531b87ca375cc
rprohan06 <74810498+rprohan06@users.noreply.github.com>
2021-08-12 13:24:15 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,60 @@
+class Quiz {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      contestant = new Contestant();
+      var contestantCountRef = await database.ref('contestantCount').once("value");
+      if(contestantCountRef.exists()){
+        contestantCount = contestantCountRef.val();
+        contestant.getCount();
+      }
+      question = new Question()
+      question.display();
+    }
+  }
+
+  play(){
+   
+question.hide();
+background("Yellow");
+fill(0);
+textSize(30);
+text("Result of quiz",240,50)
+text("-------------", 320,65)
+contestant.getPlayerInfo();
+if(allContestants !== undefined){
+  fill("blue")
+ textSize(20);
+ text ("*NOTE; Contestant who answered correct are highlighted in green color!" ,130,230, )
+  if(correctAns === allContastants[plr].answer)
+  fill("Green")
+  else
+  fill("red")
+
+  display_Answers+=30
+  textSize(20);
+  text (allContastants[plr].name +";" + allContastants[plr.answer , 250 , display_Answers])
+}
+
+
+
+    
+    
+  }
+
+}
